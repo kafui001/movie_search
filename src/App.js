778d13265b35b3d42b59b310e939e2e58b733d39
kafui001 <kafui01@yahoo.com>
2020-09-29 20:02:24 -0400

    completed the search feature

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+import SearchMovie from './SearchMovie'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovie />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
